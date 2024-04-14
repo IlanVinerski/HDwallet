85c8057555474016412a3f4c76877bca4d495278
IlanVinerski <ilanv145@gmail.com>
2024-04-14 19:44:31 +0200

    updated config

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-import path from "path";
-import express from "express";
+var express = require("express");  
+var app = express();  
 
-const app = express();
-// Express middleware to parse form data
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static("public"));
 
-// app.use(express.static("public"));
-const __dirname = path.resolve();
-console.log("__dirname=>", __dirname)
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/public/html/ethereum.html");
+})
 
-app.get("/", function (req, res) {
-  // res.sendFile(__dirname + "/public/html/ethereum.html");
-  // res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-  res.sendFile(path.resolve(__dirname, "public", "html" , "ethereum.html"))
-});
+app.get("/arbitrum", function(req, res){
+	res.sendFile(__dirname + "/public/html/arbitrum.html");
+})
 
-app.get("/arbitrum", function (req, res) {
-  // res.sendFile(__dirname + "/public/html/arbitrum.html");
-  res.sendFile(path.resolve(__dirname, "public", "html" , "arbitrum.html"))
-});
+// app.get("/index3", function(req, res){
+// 	res.sendFile(__dirname + "/public/html/index3.html");
+// })
 
-app.get("/ethereum", function (req, res) {
-  // res.sendFile(__dirname + "/public/html/ethereum.html");
-  res.sendFile(path.resolve(__dirname, "public", "html" , "ethereum.html"))
-});
+app.get("/ethereum", function(req, res){
+	res.sendFile(__dirname + "/public/html/ethereum.html");
+})
+
 
 app.listen(6500, () => {
-  console.log(`HDWallet app listening at 6500`);
-});
-
-module.exports = app
+	console.log(`HDWallet app listening at 6500`);
+})
+// app.listen(8080);
